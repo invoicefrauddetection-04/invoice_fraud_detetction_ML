b56877c8c03ac75687f3d0c5a1e0e9b6c284b34f
ShravaniB18 <shravaniborude03@gmail.com>
2026-07-28 16:43:34 +0530

    Updated main.py for uploading invoices from frontend-ui

--- Invoice_Fraud_Backend/app/main.py
+++ Invoice_Fraud_Backend/app/main.py
@@ -1,4 +1,6 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+
 from app.routes.upload import router as upload_router
 
 app = FastAPI(
@@ -6,7 +8,28 @@ app = FastAPI(
     version="1.0"
 )
 
+'''app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://192.168.2.214:5000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)'''
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(upload_router)
+
 
 @app.get("/")
 def home():
