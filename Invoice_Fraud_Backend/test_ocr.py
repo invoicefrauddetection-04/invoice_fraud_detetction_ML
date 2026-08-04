b13aab08acdcf6e3fc3e30c03feab79e1673ad8a
ShravaniB18 <shravaniborude03@gmail.com>
2026-08-05 00:35:29 +0530

    modified test code for ocr

--- Invoice_Fraud_Backend/test_ocr.py
+++ Invoice_Fraud_Backend/test_ocr.py
@@ -1,4 +1,4 @@
-from app.services.ocr_service import extract_text
+'''from app.services.ocr_service import extract_text
 
 image_path = "uploads/images/Invoice_1_page_1.jpg"
 
@@ -8,4 +8,28 @@ print("\nDetected Text\n")
 
 for item in result:
 
-    print(item)
+    print(item)''' 
+
+
+'''from paddleocr import PaddleOCR
+
+ocr = PaddleOCR(
+    lang="en",
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False
+)
+
+print("OCR Loaded Successfully!")''' 
+
+
+from paddleocr import PaddleOCR
+
+ocr = PaddleOCR(
+    lang="en",
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False
+)
+
+result = ocr.predict("uploads/images/Invoice_1_page_1.jpg")
+
+print(result)
